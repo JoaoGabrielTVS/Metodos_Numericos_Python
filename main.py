b962cec99016d177ca801ca25b00c0ef0a18c537
João Gabriel T.V.S <151951938+JoaoGabrielTVS@users.noreply.github.com>
2026-02-24 12:45:09 -0300

    Refactor resultado array and loop logic
    Refactor array initialization and update loop to append results.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import numpy as np
-resultado = np.array([1])
+resultado = np.array([])
 
 
-k = 3
+k = 300
 
 
 
 for i in range (k):    
-  resultado  = np.concatenate([[4*((-1)**i)*(1/(2*i+1))],resultado])
-  print(resultado)
+  resultado  = np.concatenate((resultado,[4*((-1)**i)*(1/(2*i+1))]))
+ 
+print(resultado.sum())
